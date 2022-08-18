fca42b40de4f4647125b1d474721b751cb3e85aa
Petr Nechaev <petr.nechaev@cogentembedded.com>
2022-08-19 00:57:03 +0300

    Improve version generation in case no git history is present

--- cmake/generate_version.cmake
+++ cmake/generate_version.cmake
@@ -10,49 +10,39 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Extract current version tag from git and set version variables
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 execute_process(
-    COMMAND git describe --tags --always
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    OUTPUT_VARIABLE GIT_HEAD_TAG
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    ERROR_QUIET
-    )
+  COMMAND git describe --tags --always
+  WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}"
+  OUTPUT_VARIABLE GIT_HEAD_TAG
+  OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
 
-execute_process(
-    COMMAND sh -c "git diff --quiet || echo -n + && git diff | md5sum | head -c 8"
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    OUTPUT_VARIABLE GIT_DIFF_TAG
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    ERROR_QUIET
-    )
-
-if (GIT_HEAD_TAG)
-    string(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+" GIT_VERSION ${GIT_HEAD_TAG})
-    set(PROJECT_VERSION_STRING "${GIT_HEAD_TAG}${GIT_DIFF_TAG}")
+if(GIT_HEAD_TAG)
+  string(REGEX MATCH "[0-9]+\\.[0-9]+(\\.[0-9]+(-[0-9]+)?)?"
+               PROJECT_NUMERIC_VERSION "${GIT_HEAD_TAG}")
+  string(REPLACE "-" "." PROJECT_NUMERIC_VERSION "${PROJECT_NUMERIC_VERSION}")
+  set(PROJECT_VERSION_STRING "${GIT_HEAD_TAG}")
 else()
-    set(PROJECT_VERSION_STRING "git${GIT_DIFF_TAG}")
+  set(PROJECT_VERSION_STRING "${PROJECT_NUMERIC_VERSION}-git")
 endif()
-string(REGEX MATCH "[0-9]+\\.[0-9]+(\\.[0-9]+)?" PROJECT_NUMERIC_VERSION ${PROJECT_VERSION_STRING})
 
-
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Generate header file if needed
-#------------------------------------------------------------------------------
-if (HEADER_SRC AND HEADER_DST)
-    configure_file(${HEADER_SRC} ${HEADER_DST} @ONLY)
+# ------------------------------------------------------------------------------
+if(HEADER_SRC AND HEADER_DST)
+  configure_file(${HEADER_SRC} ${HEADER_DST} @ONLY)
 endif()
